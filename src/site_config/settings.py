d0b10a337d2d09490dd08ca11e67ce17d8b71439
Boris Hristi <borisarroga@gmail.com>
2025-01-16 10:33:39 +0100

    Nous avons modifié l'e menu du site

--- src/site_config/settings.py
+++ src/site_config/settings.py
@@ -127,5 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Added by script
 STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
 STATIC_ROOT = BASE_DIR / "staticfiles"
